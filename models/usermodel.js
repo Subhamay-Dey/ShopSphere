bc034f235da8406b559e91d18b7967e4019f497f
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-24 18:40:05 +0530

    Add user model with Mongoose schema and MongoDB connection ✅

---
+++ models/usermodel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1.27017/Shopsphere")
+
+const userSchema = new mongoose.Schema({
+    fullname: String,
+    email: String,
+    password: String,
+    cart: {
+        type: Array,
+        default: []
+    },
+    isAdmin: Boolean,
+    orders: {
+        type: Array,
+        default: []
+    },
+    contact: Number,
+    picture: String,
+})
+
+module.exports = mongoose.model("user", userSchema)
