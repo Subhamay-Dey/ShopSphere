37adf62e916efd7e49d0bfe0ab9e6a074eba5b4a
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-25 16:03:32 +0530

    Enhance MongoDB connection handling with debug logging and update package dependencies ✅

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Shopsphere").then(function(){
-    console.log("Connected to database")
-}).catch(function(err){
-    console.log(err.message)
-})
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Shopsphere")
+  .then(function () {
+    console.log("This is console response")
+    dbgr("Connected to the database");
+  })
+  .catch(function (err) {
+    console.log(err);
+    dbgr("Failed to connect to the database");
+  });
 
 module.exports = mongoose.connection;
