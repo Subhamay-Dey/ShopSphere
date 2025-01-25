a060a350e98f8b95beb581b42cc4f5512407787b
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-25 16:08:37 +0530

    Update MongoDB connection to use config for URI and add config package dependency ✅

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
+const config = require("config");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/Shopsphere")
+  //  .connect("mongodb://127.0.0.1:27017/Shopsphere")
+  //   .connect(`${process.env.MONGODB_URI}/Shopsphere`)
+  .connect(`${config.get("MONGODB_URI")}/Shopsphere`)
   .then(function () {
-    console.log("This is console response")
+    console.log("This is console response");
     dbgr("Connected to the database");
   })
   .catch(function (err) {
