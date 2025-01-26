e57403951c2ef92cbb34edf8f8371ba90c683ac2
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-26 15:20:30 +0530

    Add user registration route and fix formatting in app.js ✅

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const ownersRouter = require('./routes/ownersRouter');
-const userRouter = require("./routes/userRouter.js")
-const productRouter = require("./routes/productRouter.js")
+const userRouter = require("./routes/userRouter.js");
+const productRouter = require("./routes/productRouter.js");
 const db = require('./config/mongooseConnection.js');
 
 const app = express();
@@ -16,10 +16,10 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.send('Express');
-})
+});
 
 app.use("/owners", ownersRouter);
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 
-app.listen(5000)
+app.listen(5000);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const usermodel = require('../models/usermodel');
 
 const router = express.Router();
 
 router.get('/', function (req, res) {
     return res.send('Users home page');
 })
+
+router.post("/register", async function(req, res) {
+    try {
+        const {fullname, email, password} = req.body;
+        const createdUser = await usermodel.create({
+            fullname,
+            email,
+            password,
+        })
+        res.send(createdUser);
+    } catch (error) {
+        console.log(error.message);
+    }
+}) 
 
 module.exports = router;
